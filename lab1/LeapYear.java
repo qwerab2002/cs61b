fc5eb53317c5626bf29fe4e3795eee5f2f9f61b8
ZZF <qwerab2002@gmail.com>
2022-05-31 00:29:21 +0800

    leapyear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -6,7 +6,15 @@ public class LeapYear {
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
-
+    public static boolean isLeapYear(int year){
+        if (year % 400 == 0) {
+            return true;
+        }
+        if (year % 100 != 0 && year % 4 == 0){
+            return true;
+        }
+        return false;
+    }
 
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
