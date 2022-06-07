ff778cfe93ad760e2905a0463d63c26b54d93bbc
ZZF <qwerab2002@gmail.com>
2022-06-07 16:38:01 +0800

    hw1 guitar

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -60,6 +60,9 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
      * Return oldest item, but don't remove it.
      */
     public T peek() {
+        if (this.isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
         return this.rb[first];
     }
 
