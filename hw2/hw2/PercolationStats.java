80f35951e0595e939ddf78cdea7a13442eac86e1
ZZF <qwerab2002@gmail.com>
2022-06-09 22:32:16 +0800

    hw2

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,40 @@
 package hw2;
-
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 public class PercolationStats {
-
+    private int N;
+    private double[] rates;
+    private static double onePercolation(int N, PercolationFactory pf) {
+        int count = 0;
+        Percolation perc = pf.make(N);
+        while (!perc.percolates()) {
+            int row = StdRandom.uniform(N);
+            int col = StdRandom.uniform(N);
+            if (perc.isOpen(row, col)) {
+                continue;
+            }
+            perc.open(row, col);
+            count++;
+        }
+        return (double) count / (N * N);
+    }
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        this.N = N;
+        rates = new double[T];
+        for (int i = 0; i < T; i++) {
+            rates[i] = onePercolation(N, pf);
+        }
+    }   // perform T independent experiments on an N-by-N grid
+    public double mean() {
+        return StdStats.mean(rates);
+    }                                           // sample mean of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(rates);
+    }                                   // sample standard deviation of percolation threshold
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(rates.length);
+    }                              // low endpoint of 95% confidence interval
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(rates.length);
+    }                                 // high endpoint of 95% confidence interval
 }
--- test_code/src/Percolation_verbose.java
+++ test_code/src/Percolation_verbose.java
@@ -3,46 +3,45 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    private boolean[] grids;
+    private int[] grids;
     private WeightedQuickUnionUF openings;
-    private WeightedQuickUnionUF perc;
     private int N;
-    private int head;
-    private int tail;
+    private int[] topRowOpen;
+    private int topRowCount;
+    private int[] bottomRowOpen;
+    private int bottomRowCount;
     private int numOpen;
+    private boolean isPercolated;
     public Percolation(int N) {
         if (N <= 0) {
             throw new IllegalArgumentException();
         }
         this.N = N;
-        grids = new boolean[N * N]; //0 means blocked, 1 means open
-        openings = new WeightedQuickUnionUF(N * N + 1); //with one extra node to detect fullness
-        perc = new WeightedQuickUnionUF(N * N + 2); //with two extra nodes to detect percolation
-        head = N * N; //all row 0 nodes connect to
-        tail = N * N + 1; //all row N-1 connect to
-        //creates an extra top row and an extra bottom row
+        grids = new int[N * N]; //0 means blocked, 1 means open
+        openings = new WeightedQuickUnionUF(N * N);
         numOpen = 0;
+        topRowOpen = new int[N];
+        topRowCount = 0;
+        bottomRowOpen = new int[N];
+        bottomRowCount = 0;
+        isPercolated = false;
     }               // create N-by-N grid, with all sites initially blocked
 
     private int idx(int row, int col) {
         return row * N + col;
     }
     private void connectNeighbors(int row, int col) {
-        if (row + 1 < N && grids[idx(row + 1, col)]) {
+        if (row + 1 < N && this.isOpen(row + 1, col)) {
             openings.union(idx(row, col), idx(row + 1, col));
-            perc.union(idx(row, col), idx(row + 1, col));
         }
-        if (row - 1 >= 0 && grids[idx(row - 1, col)]) {
-            openings.union(idx(row, col), idx(row - 1, col));
-            perc.union(idx(row, col), idx(row - 1, col));
-        }
-        if (col + 1 < N && grids[idx(row, col + 1)]) {
+        if (col + 1 < N && this.isOpen(row, col + 1)) {
             openings.union(idx(row, col), idx(row, col + 1));
-            perc.union(idx(row, col), idx(row, col + 1));
         }
-        if (col - 1 >= 0 && grids[idx(row, col - 1)]) {
+        if (row - 1 >= 0 && this.isOpen(row - 1, col)) {
+            openings.union(idx(row, col), idx(row - 1, col));
+        }
+        if (col - 1 >= 0 && this.isOpen(row, col - 1)) {
             openings.union(idx(row, col), idx(row, col - 1));
-            perc.union(idx(row, col), idx(row, col - 1));
         }
     }
     public void open(int row, int col) {
@@ -53,34 +52,54 @@ public class Percolation {
             return;
         }
         connectNeighbors(row, col);
+        grids[idx(row, col)] = 1;
         if (row == 0) {
-            openings.union(idx(row, col), head);
-            perc.union(idx(row, col), head);
+            topRowOpen[topRowCount] = idx(row, col);
+            topRowCount++;
         }
         if (row == N - 1) {
-            perc.union(idx(row, col), tail);
+            bottomRowOpen[bottomRowCount] = idx(row, col);
+            bottomRowCount++;
         }
-        grids[idx(row, col)] = true;
+        if ((!isPercolated) && isFull(row, col)) {
+            for (int i = 0; i < bottomRowCount; i++) {
+                if (openings.connected(idx(row, col), bottomRowOpen[i])) {
+                    isPercolated = true;
+                }
+            }
+        }
         numOpen++;
     }      // open the site (row, col) if it is not open already
     public boolean isOpen(int row, int col) {
         if (row < 0 || col < 0 || row >= N || col >= N) {
             throw new IndexOutOfBoundsException();
         }
-        return grids[idx(row, col)];
+        return grids[idx(row, col)] > 0;
     } // is the site (row, col) open?
     public boolean isFull(int row, int col) {
         if (row < 0 || col < 0 || row >= N || col >= N) {
             throw new IndexOutOfBoundsException();
         }
-        return openings.connected(head, idx(row, col));
+        if (!isOpen(row, col)) {
+            return false;
+        }
+        if (row == 0) {
+            return true;
+        }
+        for (int i = 0; i < topRowCount; i++) {
+            if (openings.connected(idx(row, col), topRowOpen[i])) {
+                return true;
+            }
+        }
+        return false;
     }  // is the site (row, col) full?
     public int numberOfOpenSites() {
         return numOpen;
     }           // number of open sites
     public boolean percolates() {
-        return perc.connected(head, tail);
+        return isPercolated;
     }             // does the system percolate?
     public static void main(String[] args) {
     }  // use for unit testing (not required, but keep this here for the autograder)
+
 }
